6fc9d160776fa2195eb32e4e899f5d95142ca17e
karakayamikail <112720270+karakayamikail@users.noreply.github.com>
2023-06-14 02:21:41 +0300

    Add files via upload

---
+++ work1if/js/clock.js
@@ -0,0 +1,5 @@
+var date = new Date().toLocaleString('tr-TR');
+document.getElementById("myClock").innerHTML = date;
+
+let myName = prompt('adınızı giriniz')
+document.getElementById('myName').innerHTML = myName;
